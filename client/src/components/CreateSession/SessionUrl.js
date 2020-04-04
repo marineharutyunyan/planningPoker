1c28a490c2258ce7077f9fd47c40c6aca8d22907
Marine Harutyunyan <marine.harutyunyan@helpsystems.com>
2020-04-04 23:04:41 +0400

    Added copy button functionality for sharing url.

--- client/src/components/CreateSession/SessionUrl.js
+++ client/src/components/CreateSession/SessionUrl.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useRef, useState } from 'react';
 import './CreateSession.css';
 
 export default function ShowSessionUrl({ room }) {
-    const url = `http://localhost:3000/join?room=${room}`;
+    const urlRef = useRef();
+    const [url, setUrl] = useState(`http://localhost:3000/join?room=${room}`);
+    const onCopyUrl = () => {
+        const copyText = urlRef.current;
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        const successful = document.execCommand('copy');
+        const msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Copied text command was ' + msg);
+    };
 
     return (
-      <div className="joinInnerContainer">
-        <h1 className="heading" id ="heading" >{url}</h1>
-        <div>
-            {url}
+        <div className="joinInnerContainer">
+            <h2 className="heading" id ="heading" >{url}</h2>
+            <input ref={urlRef}
+                   className="url-input"
+                   type="text"
+                   value={url}
+                   onChange={(event) => setUrl(event.target.value)}
+            />
+            <button onClick={onCopyUrl}
+                    className={'button mt-20'}
+                    type="submit"
+            >Copy</button>
         </div>
-        <button onClick={e => {}} className={'button mt-20'} type="submit">Copy</button>
-      </div>
 )};
