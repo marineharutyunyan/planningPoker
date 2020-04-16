48c184287a8f82b7c2ba8cdc5519adfd377a8798
Marine Harutyunyan <marine.harutyunyan@helpsystems.com>
2020-04-16 12:47:30 +0400

    Added users base file

---
+++ server/users.js
@@ -0,0 +1,58 @@
+const users = [];
+const history= {};
+const {ADMIN_USER_TYPE} = require('./utils');
+
+const addUser = ({ id, name, type, room }) => {
+  name = name.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+
+  const existingUser = users.find((user) => user.room === room && user.name === name);
+  if(!name || !room) return { error: 'Username and room are required.' };
+  if(existingUser) return { error: 'Username is taken.' };
+
+  const user = { id, name, type, room };
+
+  users.push(user);
+
+  return { user };
+};
+
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id);
+
+  if(index !== -1) return users.splice(index, 1)[0];
+};
+
+const getUser = (id) => users.find((user) => user.id === id);
+
+const getAdminUser = () => users.find((user) => user.type === ADMIN_USER_TYPE);
+
+const getUsersInRoom = (room) => users.filter((user) => user.room === room);
+
+const getVotingHistory = room => history[room] || [];
+
+const setVotingHistory = ({room, users, points, avaragePoint, storyNumber, storyTitle}) => {
+    if (users && users.length) {
+        const lastEstimation = {users:[], avaragePoint, storyNumber, storyTitle};
+        users.forEach((user) => {
+            lastEstimation.users.push({name: user.name, point: points[user.name]});
+        });
+
+        if (history.hasOwnProperty(room)) {
+            history[room].push(lastEstimation);
+        } else {
+            history[room] = [lastEstimation];
+        }
+    }
+    return history
+};
+
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom,
+    getAdminUser,
+    getVotingHistory,
+    setVotingHistory
+};
