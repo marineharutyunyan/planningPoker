753c249b5ded875e8d6ffbcd4f66eb3ed1963859
marinehyan <marinehyan@gmail.com>
2021-01-29 14:14:31 +0400

    Saving name of User in cache for second time easy join.

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -1,12 +1,14 @@
 import React, { useState, useRef } from 'react';
 import { Link } from "react-router-dom";
 import queryString from 'query-string';
+import Cookies from 'js-cookie'
 
 import './Join.css';
 
 export default function SignIn({ location }) {
     const buttonRef = useRef(null);
-    const [name, setName] = useState('');
+    const cachedName = Cookies.get('userName');
+    const [name, setName] = useState(cachedName || '');
     const { id: room } = queryString.parse(location.search);
 
     return (
@@ -16,6 +18,7 @@ export default function SignIn({ location }) {
             <div>
                 <input autoFocus
                     type="text"
+                    value={name}
                     placeholder="Name"
                     className="joinInput"
                     onChange={e => setName(e.target.value)}
@@ -24,7 +27,7 @@ export default function SignIn({ location }) {
             </div>
                 <Link ref={buttonRef}
                       to={`/game?id=${room}&name=${name}`}
-                      onClick={e => (!name || name.trim().length === 0 || !room) ? e.preventDefault() : null}
+                      onClick={e => (!name || name.trim().length === 0 || !room) ? e.preventDefault() : Cookies.set('userName', name)}
                 >
                     <button className={'button mt-20'} type="submit">Join</button>
                 </Link>
